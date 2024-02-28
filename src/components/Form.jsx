daae32a5d503716bac96ac9d85f0148bbd2d600b
agusmiertorre <agustinmiertorre@gmail.com>
2024-02-28 16:03:50 -0300

    Primer Commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Form.css"
+
+const Form = () => {
+  return (
+    <form action="">
+        <input type="text" name="name" />
+        <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default Form
